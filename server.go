262b1b69ccdefc2d89153c6b9ebb0d62fbfa0fc2
Kevin Y <paeynivek@gmail.com>
2023-09-26 00:53:43 -0700

    upload multiple files. upload the other contact info.

--- server.go
+++ server.go
@@ -1,49 +1,92 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
 )
 
-func uploadFile(w http.ResponseWriter, r *http.Request) {
+type contact struct {
+	Email         string
+	FirstName     string
+	Last4SSN      string
+	LastName      string
+	Message       string
+	PhoneNumber   string
+	TimeContacted string
+}
+
+func uploadFiles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("File Upload Endpoint Hit")
+
+	// Access the other form fields.
+	var c contact = contact{
+		Email:         r.FormValue("email"),
+		FirstName:     r.FormValue("firstName"),
+		Last4SSN:      r.FormValue("last4SSN"),
+		LastName:      r.FormValue("lastName"),
+		Message:       r.FormValue("message"),
+		PhoneNumber:   r.FormValue("phoneNumber"),
+		TimeContacted: time.Now().String(),
+	}
+
+	contactInfoBytes, err := json.MarshalIndent(c, "", "    ")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(string(contactInfoBytes))
 
 	// Parse our multipart form, 10 << 20 specifies a maximum
 	// upload of 10 MB files.
-	r.ParseMultipartForm(10 << 20)
-	// FormFile returns the first file for the given key `myFile`
-	// it also returns the FileHeader so we can get the Filename,
-	// the Header and the size of the file
-	file, handler, err := r.FormFile("files")
+	err = r.ParseMultipartForm(10 << 20)
 	if err != nil {
-		fmt.Println("Error Retrieving the File")
+		fmt.Println("Error Parsing Form")
 		fmt.Println(err)
 		return
 	}
-	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
 
-	// Create a temporary file within our temp-images directory that follows
-	// a particular naming pattern
-	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer tempFile.Close()
+	// Access the map of uploaded files by key.
+	files := r.MultipartForm.File["files"]
 
-	// read all of the contents of our uploaded file into a
-	// byte array
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
+	// Iterate through each uploaded file.
+	for _, fileHeader := range files {
+		// Open the file.
+		file, err := fileHeader.Open()
+		if err != nil {
+			fmt.Println("Error Retrieving the File")
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+
+		fmt.Printf("Uploaded File: %+v\n", fileHeader.Filename)
+		fmt.Printf("File Size: %+v\n", fileHeader.Size)
+		fmt.Printf("MIME Header: %+v\n", fileHeader.Header)
+
+		// Create a temporary file within our temp-images directory that follows
+		// a particular naming pattern.
+		tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer tempFile.Close()
+
+		// Copy the contents of the uploaded file to the temporary file.
+		_, err = io.Copy(tempFile, file)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Fprintf(w, "Successfully Uploaded File: %s\n", fileHeader.Filename)
 	}
-	// write this byte array to our temporary file
-	tempFile.Write(fileBytes)
-	// return that we have successfully uploaded our file!
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
+
+	fmt.Fprintf(w, "All files successfully uploaded\n")
 }
 
 func main() {
@@ -52,6 +95,6 @@ func main() {
 	fmt.Println("listening and serving on", url)
 
 	http.Handle("/", http.FileServer(http.Dir("")))
-	http.HandleFunc("/upload", uploadFile)
+	http.HandleFunc("/upload", uploadFiles)
 	http.ListenAndServe(url, nil)
 }
